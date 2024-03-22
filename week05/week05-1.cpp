ed0781aad49891ae6c67a4f71334cdd36f162afe
sweetyhank <12751090@me.mcu.edu.tw>
2024-03-22 12:02:34 +0800

    week05

---
+++ week05/week05-1.cpp
@@ -0,0 +1,17 @@
+///file-new-empty
+#include <stdio.h>
+int main()
+{
+///int a,b;
+    int a[4];///°}¦C«Å§i
+    int b[4] = {10,20,30,40};
+    a[0] =9;
+    a[1] =8;
+    a[2] =7;
+    a[3] =6;
+
+    for(int i=0; i<4;i++){
+        printf("i:%d a[i]:%d b[i]:%d\n",i,a[i],b[i]);
+    }
+
+}
